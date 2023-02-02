56eccda6640984d52b97fdc7029f9a568d0525fa
Taeyoung Won <hexter1994@gmail.com>
2023-02-03 02:15:39 +0900

    Project Init

---
+++ AppleAppStore_Clone/AppleAppStore_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleAppStore_CloneApp.swift
+//  AppleAppStore_Clone
+//
+//  Created by 원태영 on 2023/02/03.
+//
+
+import SwiftUI
+
+@main
+struct AppleAppStore_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
